e6aafcc4eb4cbdca53ae9b3dbe6db745f1261a1d
Alexander <sasacihun@gmail.com>
2024-07-25 08:55:10 +0300

    Add models

--- models.py
+++ models.py
@@ -1,26 +1,25 @@
-from sqlalchemy import Boolean, Column, ForeignKey, Integer, String
+from sqlalchemy import Boolean, Column, ForeignKey, Integer, String, Date
 from sqlalchemy.orm import relationship
 
-from .database import Base
+from database import Base
 
 
-class User(Base):
-    __tablename__ = "users"
+class DBAuthor(Base):
+    __tablename__ = "author"
 
-    id = Column(Integer, primary_key=True)
-    email = Column(String, unique=True, index=True)
-    hashed_password = Column(String)
-    is_active = Column(Boolean, default=True)
-
-    items = relationship("Item", back_populates="owner")
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String(255), nullable=False)
+    bio = Column(String(511))
+    books = Column(Boolean, default=True)
 
 
-class Item(Base):
-    __tablename__ = "items"
+class DBBook(Base):
+    __tablename__ = "book"
 
-    id = Column(Integer, primary_key=True)
-    title = Column(String, index=True)
-    description = Column(String, index=True)
-    owner_id = Column(Integer, ForeignKey("users.id"))
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String(255), nullable=False)
+    summary = Column(String(255))
+    publication_date = Column(Date, nullable=False)
+    author_id = Column(Integer, ForeignKey("author.id"))
 
-    owner = relationship("User", back_populates="items")
+    author = relationship(DBAuthor)
